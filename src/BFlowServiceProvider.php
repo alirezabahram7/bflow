8f5e776a1b650f3f01ef3b54d6b941552a9f7414
Alireza Bahram <alib327@gmail.com>
2021-07-14 12:49:07 +0430

    fix: add commands to provider

--- src/BFlowServiceProvider.php
+++ src/BFlowServiceProvider.php
@@ -3,15 +3,33 @@
 namespace BFlow;
 
 
+use BFilters\Console\Filter;
+use BFlow\Console\FlowMakeCommand;
+use BFlow\Console\StateMakeCommand;
 use BFlow\Facades\BFlow;
 use Illuminate\Support\ServiceProvider;
 
 class BFlowServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands(
+                [
+                    FlowMakeCommand::class,
+                    StateMakeCommand::class
+                ]
+            );
+        }
+    }
+
     public function register()
     {
-        $this->app->bind('bflow', function ($app) {
-            return new BFlow();
-        });
+        $this->app->bind(
+            'bflow',
+            function ($app) {
+                return new BFlow();
+            }
+        );
     }
 }
